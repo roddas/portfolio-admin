48f3e915d85d07457eb171cce0eb24fcbe780cef
Rodolfo Cabral Neves <roddas360@gmail.com>
2022-12-29 13:41:18 -0300

    roddas/update/dashboard:Change the component name from NoData to LoadingData

---
+++ src/components/Misc/LoadingData.tsx
@@ -0,0 +1,11 @@
+import './LoadingData.css';
+
+export const LoadingData = () => (
+  <>
+    <div className="container-fluid py-5">
+      <div className="d-flex justify-content-center">
+        <h1>Nada até ao momento</h1>
+      </div>
+    </div>
+  </>
+);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,64 +1,64 @@
 import { useState, useEffect } from 'react';
 import { FC } from 'react';
 import { Menu } from '../../components/Menu/Menu';
-import { NoData } from '../../components/Misc/NoData';
+import { LoadingData } from '../../components/Misc/LoadingData';
 import { Footer } from '../../components/Footer/Footer';
 import homeImage from './user.svg';
 import { JSONResponseType, AboutType } from '../../shared/Types';
 import { REMOTE_URL } from '../../shared/constants';
 
 export const Home: FC = () => {
-    const [aboutMe, setInformations] = useState<AboutType>();
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await fetch(REMOTE_URL + '/aboutme');
-                const jsonData = (await response.json()) as JSONResponseType;
-                const { statusCode, data } = jsonData;
+  const [aboutMe, setInformations] = useState<AboutType>();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(REMOTE_URL + '/aboutme');
+        const jsonData = (await response.json()) as JSONResponseType;
+        const { statusCode, data } = jsonData;
 
-                if (Number(statusCode) !== 200) {
-                    console.error(statusCode);
-                } else {
-                    setInformations(data);
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        getData();
-    }, []);
+        if (Number(statusCode) !== 200) {
+          console.error(statusCode);
+        } else {
+          setInformations(data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
 
-    if (aboutMe) {
-        const { fullName, occupation, localOfOccupation } = aboutMe;
-        return (
-            <>
-                <Menu />
-                <div className="container-fluid py-5">
-                    <div className="d-flex justify-content-center">
-                        <section className="information align-self-baseline">
-                            <h1 className="fw-lighter">
-                                Hi there, I’m
-                                <span className="fw-bolder"> {fullName}</span>
-                            </h1>
-                            <h3 className="fst-italic">
-                                a {occupation} at {localOfOccupation}.
-                            </h3>
-                        </section>
-                        <section className="image align-self-baseline">
-                            <img src={homeImage} alt="" />
-                        </section>
-                    </div>
-                </div>
-                <Footer />
-            </>
-        );
-    } else {
-        return (
-            <>
-                <Menu />
-                <NoData />
-                <Footer />
-            </>
-        );
-    }
+  if (aboutMe) {
+    const { fullName, occupation, localOfOccupation } = aboutMe;
+    return (
+      <>
+        <Menu />
+        <div className="container-fluid py-5">
+          <div className="d-flex justify-content-center">
+            <section className="information align-self-baseline">
+              <h1 className="fw-lighter">
+                Hi there, I’m
+                <span className="fw-bolder"> {fullName}</span>
+              </h1>
+              <h3 className="fst-italic">
+                a {occupation} at {localOfOccupation}.
+              </h3>
+            </section>
+            <section className="image align-self-baseline">
+              <img src={homeImage} alt="" />
+            </section>
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Menu />
+        <LoadingData />
+        <Footer />
+      </>
+    );
+  }
 };
